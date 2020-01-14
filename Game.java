6b0fafdefc75a79826f478ba3d271da335ade99d
Kryen <Kryen112@hotmail.com>
2020-01-14 14:33:01 +0100

    Changed printWelcome
    Made it for Fables of Darkness, new description.

--- Game.java
+++ Game.java
@@ -84,8 +84,9 @@ public class Game
     private void printWelcome()
     {
         System.out.println();
-        System.out.println("Welcome to the World of Zuul!");
-        System.out.println("World of Zuul is a new, incredibly boring adventure game.");
+        System.out.println("Welcome to the Fables of Darkness!");
+        System.out.println("You wake up in a dimly lit cave.");
+        System.out.println("The cave is lit by a torch in the distance.");
         System.out.println("Type '" + CommandWord.HELP + "' if you need help.");
         System.out.println();
         System.out.println(currentRoom.getLongDescription());
