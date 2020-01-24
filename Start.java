f953969adcefb6e79c45159c5b93d9e27038d876
Kryen <Kryen112@hotmail.com>
2020-01-24 15:34:31 +0100

    same with Start

--- Start.java
+++ Start.java
@@ -1,15 +1,18 @@
-import java.util.*;
-
+/**
+ * Start starts the game
+ * 
+ * Only contains the main method to play
+ *
+ * @author Stefan Jilderda and Stefan Kuppen
+ * @version 24.01.2020
+ */
 public class Start
 {
-
     private Start() {
-
     }
 
     public static void main(String[] args) { 
         Game starter = new Game();
         starter.play();
     }
-
 }
